720a3ab04decede1e39f57a0cc34f094fa3f8e80
Maksym <4ygakk@gmail.com>
2017-07-15 14:13:05 +0200

    fisrt

---
+++ template-parts/header/site-branding.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-branding">
+	<div class="wrap">
+
+		<?php the_custom_logo(); ?>
+
+		
+
+<!--
+		
+-->
+
+	</div><!-- .wrap -->
+</div><!-- .site-branding -->

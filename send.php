02f092333266713a2b32bd40cd020b66943a1545
Maksym <4ygakk@gmail.com>
2017-10-28 18:50:57 +0200

    2

---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_POST['name_client'];
+$phone = $_POST['phone_client'];
+
+
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+
+
+$name = urldecode($name);
+$phone = urldecode($phone);
+
+
+$name = trim($name);
+$phone = trim($phone);
+
+
+mail("4ygakk@gmail.com", "Заявка с сайта", "\r\n Имя:".$name. "\r\nТелефон:".$phone);
+?>
+
+
+<html>
+
+<body>
+	<div style="width: 351px;margin: 0 auto;text-align: center;">
+		<div style="margin:0 auto">
+			<h1>Спасибо! С вами свяжутся в течении 15 минут</h1>
+		</div>
+	</div>
+</body>
+</html>
